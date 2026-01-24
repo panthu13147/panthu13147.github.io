a77044b28f87e16f41b001a1081878ceb6845848
dishah <as@ass-MacBook-Pro.local>
2026-01-24 18:52:10 +0530

    Cleaned up repository: Removed external project folders

--- sniffer/Sniffer.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import sniff
-
-def packet_callback(packet):
-    # Print a summary of every packet we catch
-    print(packet.summary())
-
-print("🕵️  Starting the Sniffer... (Press Ctrl+C to stop)")
-sniff(prn=packet_callback, count=0)
